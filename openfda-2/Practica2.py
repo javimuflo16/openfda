6e46fccd633459991c6296a20dddd81adf9add32
javimuflo16 <37300300+javimuflo16@users.noreply.github.com>
2018-04-05 13:43:19 +0200

    Create Practica2.py

---
+++ openfda-2/Practica2.py
@@ -0,0 +1,19 @@
+import http.client
+import json
+
+headers = {'User-Agent':'http-client'}
+
+con = http.client.HTTPSConnection('api.fda.gov')
+con.request("GET","/drug/label.json?limit=100&search=substance_name:ASPIRIN",None,headers)#Buscamos manualmente los productos que sean aspirina
+resp = con.getresponse()
+drug_utf = resp.read().decode("utf-8")
+con.close()
+
+drugs = json.loads(drug_utf)["results"]
+for drug in drugs: #Creamos un bucle que itere sobre el json
+    if drug["openfda"]:#Si el campo openfda existe imprimimos el nombre del fabricante
+
+        manu = drug["openfda"]["manufacturer_name"][0]
+        print("El nombre del fabricante del producto es: ", manu)
+    else:
+        print("El producto no indica el nombre del fabricante")
