9425891d49d3233d835f8c7da34d6d69493486f9
javimuflo16 <37300300+javimuflo16@users.noreply.github.com>
2018-04-06 10:52:32 +0200

    Update Programa1.py

--- openfda-1/Programa1.py
+++ openfda-1/Programa1.py
@@ -1,19 +1,27 @@
 import http.client
 import json
-#Establecemos la cabecera User-Agent para determinar la informacion de la peticion
+#Informacion sobre el navegador y SO desde el que se realiza la peticion
 headers = {'User-Agent':'http-client'}
 #A continuacion, establecemos la conexion con el api
 con = http.client.HTTPSConnection('api.fda.gov')
-con.request("GET","/drug/label.json",None,headers)#Lanzamos una peticion
-resp = con.getresponse()#Recogemos la respuesta a nuestra peticion
-drug_utf = resp.read().decode("utf-8")#Decodificamos la respuesta recogida en formato json para poder manipularla
+#Lanzamos una peticion
+con.request("GET","/drug/label.json",None,headers)
+#Recogemos la respuesta a nuestra peticion
+resp = con.getresponse()
+#Decodificamos la respuesta recogida en formato json para poder manipularla
+drug_utf = resp.read().decode("utf-8")
 con.close()
 
-drugs = json.loads(drug_utf)#Tratamos el json
-drug_info = drugs["results"][0]#Creamos una variable que contenga la informacion del campo que nos interesa: 'results'
-id = drug_info["id"]#Creamos una variable que contenga los datos del id del producto
+#Tratamos el json
+drugs = json.loads(drug_utf)
+#Creamos una variable que contenga la informacion del campo que nos interesa: 'results'
+drug_info = drugs["results"][0]
+#Creamos una variable que contenga los datos del id del producto
+id = drug_info["id"]
 print("ID: ", id)
-purpose = drug_info["purpose"][0]#Tenemos en cuenta lo que hay dentro de purpose
+#Tenemos en cuenta lo que hay dentro de purpose
+purpose = drug_info["purpose"][0]
 print("Proposito del producto: ", purpose)
-manu = drug_info["openfda"]["manufacturer_name"]#Dentro del campo openfda recpgemos el campo del fabricante
+#Dentro del campo openfda recpgemos el campo del fabricante
+manu = drug_info["openfda"]["manufacturer_name"]
 print("Fabricante del producto: ", manu)
