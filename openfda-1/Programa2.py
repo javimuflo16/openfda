1cb5041b69adbaaa2fedd4b1e5c472cc932b0e59
javimuflo16 <37300300+javimuflo16@users.noreply.github.com>
2018-04-06 10:54:19 +0200

    Update Programa2.py

--- openfda-1/Programa2.py
+++ openfda-1/Programa2.py
@@ -1,16 +1,17 @@
 import http.client
 import json
-#Estalecemos la cabecera User-Agent para determinar la información de la petición
 headers = {'User-Agent':'http-client'}
 
 con = http.client.HTTPSConnection('api.fda.gov')
-con.request("GET","/drug/label.json?limit=10",None,headers)#Con el parámetro limit acotamos a 10 resultados
+#Con el parámetro limit acotamos a 10 resultados
+con.request("GET","/drug/label.json?limit=10",None,headers)
 resp = con.getresponse()
 drug_utf = resp.read().decode("utf-8")
 con.close()
 
 drugs = json.loads(drug_utf)
-for drug in range(len(drugs['results'])):#Creamos un bucle for que itere sobre cada uno de los campos de 'results' para obtener los id de cada medicamento
+#Creamos un bucle for que itere sobre cada uno de los campos de 'results' para obtener los id de cada medicamento
+for drug in range(len(drugs['results'])):
     drug_info = drugs['results'][drug]
 
     id = drug_info["id"]
